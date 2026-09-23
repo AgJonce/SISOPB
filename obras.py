ef952779f57d7af9730f4c3c37d5148ca2d180d5
AgJonce <waltairmoraes@gmail.com>
2026-09-23 19:26:37 -0300

    Add import for streamlit_folium and geopy

--- obras.py
+++ obras.py
@@ -6,6 +6,7 @@ import streamlit as st
 import pandas as pd
 import folium
 
+
 from streamlit_folium import st_folium
 from geopy.geocoders import Nominatim
 from datetime import datetime, timedelta
